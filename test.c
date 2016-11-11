454828fde512f2c11e596ebb328528487026b817
Stephane LE LIEGARD <sle-lieg@e3r8p13.42.fr>
2016-11-12 00:00:47 +0100

    libft.a

--- test.c
+++ test.c
@@ -1,34 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	*ft_init(void)
+char	**ft_init(void)
 {
-	char *ptr;
+	char **ptr;
+	int i;
 
+	i = 0;
 	ptr = malloc(sizeof(*ptr) * 5);
+	for (; i < 5; i++)
+		ptr[i] = malloc(sizeof(**ptr) * 5);
 	return (ptr);
 }
 
-void	ft_fill(char *ptr)
+void	ft_fill(char **ptr)
 {
 	int i;
+	int j;
 
-
+	for (i = 0; i < 5; i++)
+		for (j = 0; j < 5; j++)
+			ptr[i][j] = i + j + 'a';
 }
 
 int		main()
 {
-	char *str;
+	char **str;
 	int i;
 
 	str = ft_init();
 	ft_fill(str);
 	for (i = 0; i < 5; i++)
 	{
-		str[i] = i + 'a';
+//		str[i] = i + 'a';
 		printf("addresse de str + i : %p\n", &str + i);
 		printf("valeur de str + i : %p\n", str + i);
-		printf("valeur pointee par str + i : %c\n", *(str + i));
+		printf("valeur pointee par str + i : %s\n", *(str + i));
 	}
 	if (!str)
 	{
